2db2ac53ceeb678c3e63647846b464ce53a0bffa
foued <rezgui.foued@gmail.com>
2023-04-10 13:05:19 +0200

    Update InstallSoftware.sh

--- InstallSoftware.sh
+++ InstallSoftware.sh
@@ -1,11 +1,15 @@
-Le scripting c'est une suite d'instruction , pour répondre a un besoin.
-cat /etc/shells ==> permet d'afficher les shells utiliser par la machine.
-#! /bin/bash => elle permet de spécifier le bash shell a utiliser.
-Comment exécuter un script in linux:
-
-vi demo.sh creéer votre script.
-#!/bin/bash echo "Hello World"
-
-chmod +x demo.sh => on change de permission .
-
-Pour éxecuter : ./demo.sh pour éxécuter le shell script.
+#!/bin/bash
+echo "script to install git "
+echo "Installation started"
+if [ "$(uname)" == "Linux" ];
+then
+        echo "this is linux box,installing git"
+        yum install git -y
+elif [ "$(uname)" == "Darwin" ]; // ca veut dire que c'est un macos notre os et pas linux
+then
+        echo "this is not linux box"
+        echo "this is Macos"
+        brew install git
+else  // ici si notre os n'est pas ni linux , ni macos
+        echo "not installing"
+fi
