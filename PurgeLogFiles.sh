e93362ae90512eeb7a7a8d1b05b9b3c2a0504aff
foued <rezgui.foued@gmail.com>
2023-04-17 17:10:28 +0200

    Update PurgeLogFiles.sh

--- PurgeLogFiles.sh
+++ PurgeLogFiles.sh
@@ -1,1 +1,19 @@
+# thi programme will delete logs files older than 30 days.
 
+# Author FOued Rezgui
+# Version 1.0
+
+# ! /bin/bash
+
+echo "this script delete files which are older tan 30 days
+
+path = $path 
+echo $path
+
+find $path -mtime + 30 exec rm -rf {}
+if [[ $? -eq 0]]; // ca veut dire si la commande si dessus est executé avec succés
+then
+ echo "Files are successufly deleted"
+else 
+ echo " deletetion was having some issue"
+fi 
